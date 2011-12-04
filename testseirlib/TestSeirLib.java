96038a8ec8c4a96ec1b0c6c0e95886c4b6a8ef20
Anthony Gallagher <anthonyghr@gmail.com>
2011-12-04 12:18:04 -0500

    Updating the Java demo file to display incidence curve

--- testseirlib/TestSeirLib.java
+++ testseirlib/TestSeirLib.java
@@ -30,14 +30,18 @@ public class TestSeirLib {
         // TODO code application logic here
         SeirRunner seirRunner = new SeirRunner(seirParams);
         
-        HashMap<String, Number[]> seirResult = seirRunner.runSeir(100L);
-        Number[] sCurve = seirResult.get("sCurve");
-        Number[] eCurve = seirResult.get("eCurve");
-        Number[] iCurve = seirResult.get("iCurve");
-        Number[] rCurve = seirResult.get("rCurve");
+        HashMap<String, Object> seirResult = seirRunner.runSeir(100L);
+        Number[] sCurve = (Number[])seirResult.get("sCurve");
+        Number[] eCurve = (Number[])seirResult.get("eCurve");
+        Number[] iCurve = (Number[])seirResult.get("iCurve");
+        Number[] rCurve = (Number[])seirResult.get("rCurve");
+        Number[] incidenceCurve = (Number[])seirResult.get("incidenceCurve");
+        Number numberInfected = (Number)seirResult.get("numberInfected");
         System.out.println("sCurve(" + sCurve.length + "): " + Arrays.toString(sCurve));
         System.out.println("eCurve(" + eCurve.length + "): " + Arrays.toString(eCurve));
         System.out.println("iCurve(" + iCurve.length + "): " + Arrays.toString(iCurve));
         System.out.println("rCurve(" + rCurve.length + "): " + Arrays.toString(rCurve));
+        System.out.println("incidenceCurve(" + incidenceCurve.length + "): " + Arrays.toString(incidenceCurve));
+        System.out.println("numberInfected: " + numberInfected);
     }
 }
